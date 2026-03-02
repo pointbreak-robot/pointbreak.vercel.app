5297a1109488ab366e5067b485011459a4572803
smsreepathi7@gmail.com <smsreepathi7@gmail.com>
2026-03-01 16:09:13 -0800

    remove base path for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://circuitsurfers-robotics.github.io',
-    base: '/circuitsurfers.github.io',
+    site: 'https://circuitsurfers.vercel.app',
 });
